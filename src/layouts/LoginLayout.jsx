d7de5f57ffa8100f79c14f343e7f6a804400f1d2
brother-antony <elmer.antony12@gmail.com>
2025-05-08 22:27:05 -0500

    finish prueba modificacion image

--- src/layouts/LoginLayout.jsx
+++ src/layouts/LoginLayout.jsx
@@ -2,7 +2,12 @@ export default function LoginLayout({ children }) {
     return (
         <section className='login'>
             <div className="container">
-                <div className='login__left select-none hide-for-mobile'><img src="./portada-login.png" alt="portada rimac" /></div>
+                <div className='login__left select-none hide-for-mobile'>
+                    <picture>
+                        <source srcSet="./portada-login-mobile.webp" type="image/webp" media="(max-width: 768px)" />
+                        <img src="./portada-login-desktop.webp" alt="portada rimac" loading="lazy" />
+                    </picture>
+                </div>
 
                 <div className="login__right">
                     <div className='login__right--mobil'>
@@ -15,7 +20,11 @@ export default function LoginLayout({ children }) {
                             </div>
                         </div>
 
-                        <div className='hide-for-desktop login__right--mobil--img'><img src="./portada-login.png" alt="" /></div>
+                        <div className='hide-for-desktop login__right--mobil--img'>
+                            <picture>
+                                <img src="./portada-login-mobile.webp" alt="portada rimax" loading="lazy" />
+                            </picture>
+                        </div>
                     </div>
 
                     <div className='w-full h-[1px] bg-[var(--gray30)] mt-6 hide-for-desktop'></div>
@@ -24,10 +33,11 @@ export default function LoginLayout({ children }) {
                 </div>
             </div>
             
-            <img src="./blur-asset.png" className='absolute right-0 top-0 select-none hide-for-mobile' />
-            <img src="./blur-asset1.png" className='absolute right-0 top-0 select-none hide-for-desktop' />
-            <img src="./blur-asset-left.png" className='absolute left-0 bottom-0 select-none hide-for-mobile' />
-            <img src="./blur-asset-left2.png" className='absolute left-0 bottom-0 select-none hide-for-desktop' />
+            {/* Efecto blur decorativo */}
+            <img src="./blur-asset.png" className='absolute right-0 top-0 select-none hide-for-mobile' alt="" />
+            <img src="./blur-asset1.png" className='absolute right-0 top-0 select-none hide-for-desktop' alt="" />
+            <img src="./blur-asset-left.png" className='absolute left-0 bottom-0 select-none hide-for-mobile' alt="" />
+            <img src="./blur-asset-left2.png" className='absolute left-0 bottom-0 select-none hide-for-desktop' alt="" />
         </section>
     )
 }
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -17,7 +17,7 @@ export default function NotFound() {
                 <i className='fad fa-plug text-[54px] text-[var(--red5)]'></i>
                 <div className='text-[var(--neutrals7)] text-8xl font-bold my-4'>404</div>
                 <div className='text-[var(--gray60)] font-bold text-2xl'>No routes matched location</div>
-                <div className='mt-6 text-[var(--neutrals6)] text-base'>La página que estás buscando no se encontró. <br /> Comprueba la URL o regresa a la <button type='button' onClick={() => navigate(-1)} className='underline text-[var(--red5)] font-bold'>Página de Inicio.</button></div>
+                <div className='mt-6 text-[var(--neutrals6)] text-base'>La página que estás buscando no se encontró. <br /> Comprueba la URL o regresa al <button type='button' onClick={() => navigate(-1)} className='underline text-[var(--red5)] font-bold'>Inicio de sesión</button></div>
             </section>
 
             <Footer/>
